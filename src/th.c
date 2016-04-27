b8b58fea0f56c8b2e4e929c2ce9a262dfef852af
Jorne Roefs <jorneroefs@gmail.com>
2016-04-27 21:05:06 +0200

    Initial version

---
+++ src/th.c
@@ -0,0 +1,36 @@
+
+#include <stdio.h>
+
+#include <wiringPi.h>
+#include <maxdetect.h>
+
+#define RHT03_PIN       7
+#define RETRIES         3
+
+int main()
+{
+  int temp, rh, loop, status;
+
+  // Setup WiringPi
+  wiringPiSetup () ;
+  piHiPri       (55) ;
+
+  // Read sensor
+  temp = rh = -1;
+  loop=RETRIES;
+
+  status = readRHT03 (RHT03_PIN, &temp, &rh);
+  while ((!status) && loop--)
+  {
+      printf("-Retry-");
+      fflush(stdout);
+      delay(3000);
+      status = readRHT03 (RHT03_PIN, &temp, &rh);
+  }
+
+  fflush(stdout);
+
+  // Print measurement
+  printf("Current Temp: %5.1f\n",(temp / 10.0)) ;
+
+}
