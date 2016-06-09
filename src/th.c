108a158f86b92368b1d6f8847a521c2a046c401e
Jorne Roefs <jorneroefs@gmail.com>
2016-06-09 08:00:25 +0200

    Retry on invalid readings

--- src/th.c
+++ src/th.c
@@ -1,21 +1,30 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <wiringPi.h>
 #include <maxdetect.h>
 
 #define DEFAULT_PIN 7
 #define RETRIES     3
-#define TEMP_MIN  -40
+#define TEMP_MIN (-40)
 #define TEMP_MAX   80
 #define RHUM_MIN    0
 #define RHUM_MAX  100
 
 int main(int argc, char *argv[])
 {
-  int pin, temp, rh, loop, success, status;
+  int pin, raw_temp, raw_rh, loop, success, status;
+  double temp, rh;
 
-  pin = argv[1] || DEFAULT_PIN;
+  if (argc >= 2) {
+    pin = atoi(argv[1]);
+    fprintf(stderr, "Using pin %d\n", pin);
+  }
+  else {
+    pin = DEFAULT_PIN;
+    fprintf(stderr, "Using default pin %d\n", pin);
+  }
 
   // Setup WiringPi
   wiringPiSetup();
@@ -23,28 +32,28 @@ int main(int argc, char *argv[])
 
   // Read sensor
   temp = rh = -1;
-  loop = RETRIES + 1;
-  success = 0;
+  loop = success = 0;
 
-  while ((!success) && loop--) {
-      if (loop < RETRIES) delay(3000);
+  while ((!success) && (loop++ <= RETRIES + 1)) {
+    delay(loop * 1000);
 
-      status = readRHT03 (pin, &temp, &rh);
-      temp = temp / 10.0;
-      rh = rh / 10.0;
+    fprintf(stderr, "Reading... (%d) ", loop);
+    status = readRHT03 (pin, &raw_temp, &raw_rh);
+    temp = raw_temp / 10.0;
+    rh = raw_rh / 10.0;
 
-      if (!status) {
-        fprintf(stderr, "Read failed\n");
-      }
-      else if (temp < TEMP_MIN || temp > TEMP_MAX) {
-        fprintf(stderr, "Invalid temperature\n");
-      }
-      else if (rh < RHUM_MIN || rh > RHUM_MAX) {
-        fprintf(stderr, "Invalid relative humidity\n");
-      }
-      else {
-        success = 1;
-      }
+    if (!status) {
+      fprintf(stderr, "Read failed\n");
+    }
+    else if (temp < TEMP_MIN || temp > TEMP_MAX) {
+      fprintf(stderr, "Invalid temperature\n");
+    }
+    else if (rh < RHUM_MIN || rh > RHUM_MAX) {
+      fprintf(stderr, "Invalid relative humidity\n");
+    }
+    else {
+      success = 1;
+    }
   }
 
   fflush(stdout);
